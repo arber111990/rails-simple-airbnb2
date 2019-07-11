98d18af8eb55d4f5421457b8ba24b7455c8dc8e0
Arber Zyb <arber111990@gmail.com>
2019-07-11 20:01:56 +0100

    CRUD 2

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -4,10 +4,38 @@ class FlatsController < ApplicationController
   end
 
   def show
-    @flats = Flat.find(params[:id])
+    @flat = Flat.find(params[:id])
   end
 
   def new
-    @flats = Flat.new
+    @flat = Flat.new
   end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def edit
+
+  end
+
+  def update
+    @flat = Flat.find(params[:id])
+    @flat.update(flat_params)
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to restaurant_path(@restaurant)
+  end
+end
+
+private
+
+def flat_params
+  params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
 end
